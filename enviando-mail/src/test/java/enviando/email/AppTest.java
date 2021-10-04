96fef86c3f986cb0331b7b26fc3fc507e6227e28
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2021-10-04 09:44:52 -0300

    Criacao Projeto

---
+++ enviando-mail/src/test/java/enviando/email/AppTest.java
@@ -0,0 +1,25 @@
+package enviando.email;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class AppTest { 
+
+
+
+	@org.junit.Test
+	public void testeEmail(){
+		
+		
+	}
+		
+}
